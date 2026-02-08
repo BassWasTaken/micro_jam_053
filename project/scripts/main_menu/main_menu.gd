1f52df3bb2c40e9bf57410e7c71bc86227d80caf
Jaep Cuperus <jaepcuperus@hotmail.com>
2026-02-08 10:22:29 +0100

    level transition wip

--- project/scripts/main_menu/main_menu.gd
+++ project/scripts/main_menu/main_menu.gd
@@ -1,9 +1,10 @@
 extends Node
 
 @export var start_option: MenuOption
+@export var level_transition: LevelTransition
 
 func _ready():
 	start_option.on_grab.connect(start)
 
 func start():
-	print("start")
+	pass
--- project/scripts/main_scene.gd
+++ project/scripts/main_scene.gd
@@ -1,8 +1,28 @@
 extends Node2D
 
+@export var level_transition: LevelTransition
+@export var initial_scene: PackedScene
+@export var scene_holder: Node2D
+
 func _ready():
-	LevelManager.load_level($World, "1", 12)
-	UserInterface.go_to_main_menu_signal.connect(func():
-		# TODO replace this when the main menu is implemented
-		LevelManager.load_level($World, "1", 12)
-	)
+	SceneManager.on_switch.connect(on_scene_load)
+	SceneManager.transition_to_scene(initial_scene)
+
+	# LevelManager.load_level($World, "1", 12)
+	# UserInterface.go_to_main_menu_signal.connect(func():
+	# 	# TODO replace this when the main menu is implemented
+	# 	LevelManager.load_level($World, "1", 12)
+	# )
+
+func on_scene_load(scene: PackedScene, transition: bool):
+	if transition:
+		level_transition.fade_in()
+		await level_transition.faded_in
+
+	print("done fadin")
+	# clear up old scenes
+	for child in scene_holder.get_children():
+		child.queue_free()
+	var scene_instance = scene.instantiate()
+	scene_holder.add_child(scene_instance)
+	level_transition.fade_out();
